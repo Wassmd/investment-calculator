cc74eff3908fa7489318ec8068ca922a19433608
Wasim <mohammed.wasimuddin@rewe-group.com>
2025-09-21 22:38:20 +0200

    Add UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { Header } from './header/header';
+import { Calculator} from './calculator/calculator';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Calculator],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('investment-calculator');
 }
